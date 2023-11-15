03af6c05b4e20d1568c3f33035b74fc9a82722c2
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-15 19:08:02 +0100

    12

--- ej3/claseynombres.cpp
+++ ej3/claseynombres.cpp
@@ -11,3 +11,8 @@ namespace Geometria {
         float calcularArea() {
             return 3.14159 * radio * radio;
         }
+        float calcularPerimetro() {
+            return 2 * 3.14159 * radio;
+        }
+    };
+}
