9619139c18cddf66f3c8a65d79500948663d1d23
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-15 18:49:38 +0100

    3

---
+++ claseynombres.cpp
@@ -0,0 +1,3 @@
+//
+// Created by alber_wzgn3ys on 15/11/2023.
+//
---
+++ ej2/triangulo.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+namespace Geometria {
+    float calcularAreaTriangulo(float base, float altura) {
+        return 0.5 * base * altura;
+    }
+}
+
