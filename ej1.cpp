ccd02d70f120eab5c7ecc818346d08e4280d2154
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-15 18:38:20 +0100

    2

--- ej1.cpp
+++ ej1.cpp
@@ -10,4 +10,12 @@ namespace Matematicas {
     }
 }
 
+int main() {
+    int resultado_suma = Matematicas::suma(28, 14);
+    int resultado_resta = Matematicas::resta(56, 43);
 
+    std::cout << "Resultado suma: " << resultado_suma << std::endl;
+    std::cout << "Resultado resta: " << resultado_resta << std::endl;
+
+    return 0;
+}
