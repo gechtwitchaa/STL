fe56454e95e9d322f988b73134a1a263b522312b
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-15 19:03:53 +0100

    9

--- circulo.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-namespace Geometria {
-    float calcularAreaCirculo(float radio) {
-        return 3.14f * radio * radio;
-    }
-}
--- ej2/triangulo.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-namespace Geometria {
-    float calcularAreaTriangulo(float base, float altura) {
-        return 0.5f * base * altura;
-    }
-}
-
